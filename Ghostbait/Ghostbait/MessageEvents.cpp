04fa4fa8cfb08ca4f861cf21511ad4b1a84c929b
FSPeterJ <Klypto@KlyptoTech.com>
2018-01-08 15:22:00 -0500

    Moved MessageEvents to respective folders

--- Ghostbait/Ghostbait/MessageEvents.cpp
+++
@@ -1,9 +0,0 @@
-#include "MessageEvents.h"
-
-std::vector<std::function<void()>> MessageEvents::event_func::delegates;
-
-std::unordered_map<int, MessageEvents::event_func> MessageEvents::eventmap;
-
-MessageEvents::MessageEvents() { }
-
-MessageEvents::~MessageEvents() {}

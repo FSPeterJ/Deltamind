eee0269b97ba79d629f7a4fa9683ce126da5a183
Marshall Yeung <MSYeung@fullsail.edu>
2018-01-08 16:34:18 -0500

    Open VR Init

---
+++ Ghostbait/Ghostbait/Source Files/VRManager.cpp
@@ -0,0 +1,28 @@
+#include "VRManager.h"
+#include "Console.h"
+
+
+VRManager::VRManager()
+{
+}
+
+
+VRManager::~VRManager()
+{
+	if (pVRHMD) {
+		vr::VR_Shutdown();
+	}
+}
+
+bool VRManager::init()
+{
+	vr::EVRInitError error = vr::VRInitError_None;
+	pVRHMD = vr::VR_Init(&error, vr::VRApplication_Scene);
+
+	if (error != vr::VRInitError_None) {
+		pVRHMD = nullptr;
+		Console::Write("Unable to initialize VR: ");
+		Console::WriteLine(vr::VR_GetVRInitErrorAsSymbol(error));
+		return false;
+	}
+}

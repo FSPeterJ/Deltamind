29bb9dd547188789972110ccafbd86b86ea6c8c9
Austin Perry <AWPerry@fullsail.edu>
2018-01-24 09:20:27 -0500

    Fixed memory manager crash

--- Ghostbait/Ghostbait/Header Files/Pool.h
+++ Ghostbait/Ghostbait/Header Files/Pool.h
@@ -54,7 +54,7 @@ class Pool:
 	T* elements;
 	size_t pool_size;
 public:
-	Pool(size_t size = (size_t) 128) : pool_size((size_t)2000), elements((T*) memManage->RequestMemory(pool_size, sizeof(T)))
+	Pool(size_t size = (size_t) 128) : pool_size((size_t)size), elements((T*) memManage->RequestMemory(size, sizeof(T)))
 	{
 		inactiveList.resize(pool_size);
 		for(size_t i = 0; i < pool_size; ++i)

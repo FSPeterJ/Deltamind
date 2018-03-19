1e0238dc1d066e08370d39c20e781774919c80b7
Carter Ceely <carterceely@gmail.com>
2018-03-19 10:54:40 -0400

    Font index file and picture loading added

--- Ghostbait/Ghostbait/Header Files/Font.h
+++ Ghostbait/Ghostbait/Header Files/Font.h
@@ -2,22 +2,34 @@
 #include <string>
 #include <unordered_map>
 
+struct ID3D11Texture2D;
+struct ID3D11ShaderResourceView;
+struct ID3D11Device;
+struct ID3D11DeviceContext;
+
 struct CharPos {
-	float startU, startV, endU, endV;
+	float startU, endU, size;
 	CharPos();
-	CharPos(float _startU, float _startV, float _endU, float _endV);
+	CharPos(float _startU, float _endU, float _size);
 };
 
 class Font {
-	std::string fileName;
 	std::unordered_map<char, CharPos> charMap;
+	ID3D11Texture2D* tex;
+	ID3D11ShaderResourceView* srv;
+	
 
+	bool LoadIndexFile(std::string _fileName);
+	bool LoadTexture(std::string _fileName, ID3D11Device* device, ID3D11DeviceContext* context);
 public:
 	Font();
-	Font(std::string _fileName);
+	Font(std::string _fileName, std::string _texturePath, ID3D11Device* device, ID3D11DeviceContext* context);
+	~Font();
 
 	inline void SetCharPos(char c, CharPos pos) { charMap[c] = pos; }
 	const CharPos GetCharPos(char c);
-	
-	inline const CharPos InvalidCharPos() const { return CharPos(-1, -1, -1, -1); }
+
+	inline const CharPos InvalidCharPos() const { return CharPos(-1, -1, -1); }
+
+	inline const ID3D11ShaderResourceView* GetShaderResourceView() { return srv; }
 };
